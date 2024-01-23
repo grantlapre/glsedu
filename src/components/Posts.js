651be06bb5726e0b8b89e8a5445cb48baa24e735
Lawrie Grant-Lapre <lawr4life@gmail.com>
2024-01-24 00:42:59 +0800

    e34

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -42,44 +42,22 @@ const Posts = () => {
         },
         {
         title: "Data Structure ",
-        body: `There are many real-life examples of
-        a stack. Consider an example of plates stacked
-        over one another in the canteen. The plate
-        which is at the top is the first one to be
-        removed, i.e. the plate which has been placed
-        at the bottommost position remains in the
-        stack for the longest period of time. So, it
-        can be simply seen to follow LIFO(Last In
-        First Out)/FILO(First In Last Out) order.`,
-        author: "Suresh Kr",
+        body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+        author: "Lawrie Student CSU - Masters of Cyber Security",
         blogImg:
             "https://media.geeksforgeeks.org/img-practice/banner/coa-gate-2022-thumbnail.png",
         },
         {
         title: "Algorithm",
-        body: `The word Algorithm means “a process
-        or set of rules to be followed in calculations
-        or other problem-solving operations”. Therefore
-        Algorithm refers to a set of rules/instructions
-        that step-by-step define how a work is to be
-        executed upon in order to get the expected
-        results. `,
-        author: "Monu Kr",
+        body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+        author: "Lawrie Student CSU - Masters of Cyber Security",
         blogImg:
             "https://media.geeksforgeeks.org/img-practice/banner/google-test-series-thumbnail.png",
         },
         {
         title: "Computer Network",
-        body: `An interconnection of multiple devices,
-        also known as hosts, that are connected using
-        multiple paths for the purpose of sending/
-        receiving data media. Computer networks can
-        also include multiple devices/mediums which
-        help in the communication between two different
-        devices; these are known as Network devices
-        and include things such as routers, switches,
-        hubs, and bridges. `,
-        author: "Sonu Kr",
+        body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+        author: "Lawrie Student CSU - Masters of Cyber Security",
         imgUrl:
             "https://media.geeksforgeeks.org/img-practice/banner/cp-maths-java-thumbnail.png",
         },

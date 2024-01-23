8f5fa1844eb28f0c75d24f30e9718b21e119db7b
Lawrie Grant-Lapre <lawr4life@gmail.com>
2024-01-23 23:23:11 +0800

    e21

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import React from 'react';
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-
-import homep from "./pages/homep";
-import cybs from "./pages/cybs";
-import antm from "./pages/antm";
-import fraf from "./pages/fraf";
-import clov from "./pages/clov";
-import MyButton from "./components/MyButton";
+import CardExample from './CardExample';
 
 
 
@@ -20,24 +12,7 @@ function App () {
   return (
 
 
-  <BrowserRouter>
-    <MyButton to="" />
-    <MyButton to="cybse"/>
-    <MyButton to="antmo" />
-    <MyButton to="frafi" />
-    <MyButton to="clvir" />
-        <Routes>
-            <Route path="/" element={<homep />} />
-            <Route path="cybse"
-                element={<cybs />} />
-            <Route path="antmo"
-                element={<antm />} />
-            <Route path="frafi"
-                element={<fraf />} />
-            <Route path="clvir"
-                element={<clov />} />
-        </Routes>
-    </BrowserRouter>
+  <CardExample />
 
   )
   };

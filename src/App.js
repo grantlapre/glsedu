c7c6f936b4daba5731323ce0e6ea7ab46ec9dff7
Lawrie Grant-Lapre <lawr4life@gmail.com>
2024-01-23 20:00:55 +0800

    six

--- src/App.js
+++ src/App.js
@@ -1,46 +1,13 @@
 import React, { useState } from 'react';
 
-import Card from 'react-bootstrap/Card';
-import Toast from 'react-bootstrap/Toast';
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
+
+import cards from './cards';
 
 import './App.css';
 
-const ExampleToast = ({ children }) => {
-  const [show, toggleShow] = useState(true);
-
-  return (
-    <>
-      {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
-      <Toast show={show} onClose={() => toggleShow(false)}>
-        <Toast.Header>
-          <strong className="mr-auto">Cyber Security</strong>
-        </Toast.Header>
-        <Toast.Body>{children}</Toast.Body>
-      </Toast>
-      <Toast show={show} onClose={() => toggleShow(false)}>
-        <Toast.Header>
-          <strong className="mr-auto">AML/CFT</strong>
-        </Toast.Header>
-        <Toast.Body>{children}</Toast.Body>
-      </Toast>
-    </>
-  );
-};
 
 const App = () => (
-  <Container className="p-3">
-    <Card>
-      <h1 className="header">Welcome To GLSDefi Educate</h1>
-      <ExampleToast>
-        We now have Toasts
-        <span role="img" aria-label="tada">
-          🎉
-        </span>
-      </ExampleToast>
-    </Card>
-  </Container>
+  <cards/>
 );
 
 export default App;

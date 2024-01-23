590ccf4664d1376ce977f2f9f872f64011598543
Lawrie Grant-Lapre <lawr4life@gmail.com>
2024-01-24 00:39:35 +0800

    e33

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,7 +5,7 @@
 import React from "react";
 import "./Post.css";
 const Post = ({ post: { title, body,
-imgUrl, author }, index }) => {
+blogImg, author }, index }) => {
 return (
 	<div className="post-container">
 	<h1 className="heading">{title}</h1>

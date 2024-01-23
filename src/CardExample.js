512150b45b35f5662da66cef0f443a1d2f3b0bf7
Lawrie Grant-Lapre <lawr4life@gmail.com>
2024-01-23 20:51:35 +0800

    ten

--- src/CardExample.js
+++ src/CardExample.js
@@ -5,6 +5,8 @@ import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import cs1 from './cs1.png';
 import am1 from './am1.png';
+import ff1 from './ff1.png';
+import cv1 from './cv1.png';
 
 import './App.css';
 
@@ -37,6 +39,26 @@ financing offenses, plus enforcement power that provides for freezing, seizing a
 confiscation of the proceeds of crime and terrorist funding. 
         </Card.Text>
         <Button variant="primary">AML/CFT articles & news</Button>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={ff1} width="180" height="100" />
+      <Card.Body>
+        <Card.Title>Fraud & Financial Crime</Card.Title>
+        <Card.Text>
+        It can include fraudulent sales of goods and services, investment scams, phishing, and romance scams, among others. Identity theft: This occurs when someone uses another person's identity to obtain goods, services, or financial gain.
+        </Card.Text>
+        <Button variant="primary">Fraud&FinCrime articles & news</Button>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={cv1} width="180" height="100" />
+      <Card.Body>
+        <Card.Title>Cloud & Virtualisation</Card.Title>
+        <Card.Text>
+        Virtualization is a technology that allows users to create virtual servers, networks, and storage in the cloud. Cloud computing is a more comprehensive idea that refers to delivering numerous services, including computing resources ...e.g., virtual machines..., storage, databases, and applications, over the Internet.
+        </Card.Text>
+        <Button variant="primary">C&V articles & news</Button>
       </Card.Body>
     </Card>
   </Container>

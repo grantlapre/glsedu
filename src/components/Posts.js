89d7967a9c52e1fb3ad5da7b58016749a5c44c54
Lawrie Grant-Lapre <lawr4life@gmail.com>
2024-01-24 11:38:04 +0800

    e36

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -12,7 +12,7 @@ const Posts = () => {
 
     const blogPosts = [
         {
-        title: "The Dark Side of Digital Age: Cyber Spying and Its Tools",
+        title: "The Dark Side of Digital Age: Cyber Spying and Its Tools | January 23 2024",
         body: ` 
 
         In the labyrinthine world of digital technology, where data is the new gold and safeguarding it is akin to a modern-day crusade, cyber spying has emerged as a chilling reality. This shadowy practice isn't just confined to rogue hackers in dimly lit basements; even governments and security companies are implicated, using sophisticated tools that blur the line between protection and invasion.
@@ -38,12 +38,12 @@ const Posts = () => {
         To your safety`
         
         ,
-        author: "Lawrie Student CSU - Master of Cyber Security - Jan-Mar24",
+        author: "Lawrie Student CSU - Master of Cyber Security",
         blogImg:
             cs1,
         },
         {
-        title: "The Hidden Dangers of Backdoors in Computer Codes: A Deep Dive into Cybersecurity",
+        title: "The Hidden Dangers of Backdoors in Computer Codes: A Deep Dive into Cybersecurity | January 24 2024",
         body: `
 
         Every so often, a chilling question reverberates through the hushed corridors of the digital world, "How much can we trust the operating systems and applications that we use?" This is not an idle query. It's a vital concern that demands our attention. After all, we're living in an era where our lives are intricately woven with technology.
@@ -66,7 +66,7 @@ const Posts = () => {
         
         To your online safety,
         `,
-        author: "Lawrie Student CSU - Master of Cyber Security- Jan-Mar24",
+        author: "Lawrie Student CSU - Master of Cyber Security",
         blogImg:
             bd1,
         },
